7da3eb38edab236e4009f476b3192f14bc934021
rawray7 <rawray7@gmail.com>
2016-01-13 21:52:27 -0800

    try 1st strategy for adding public experiments

---
+++ public.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * experiments.php
+ *
+ * @author Nicolas CARPi <nicolas.carpi@curie.fr>
+ * @copyright 2012 Nicolas CARPi
+ * @see http://www.elabftw.net Official website
+ * @license AGPL-3.0
+ * @package elabftw
+ */
+
+/**
+ * Entry point for all experiment stuff
+ *
+ */
+require_once 'inc/common.php';
+$page_title = ngettext('Experiment', 'Experiments', 2);
+$selected_menu = 'Experiments';
+require_once 'inc/head.php';
+
+// add the chemdoodle stuff if we want it
+echo addChemdoodle();
+
+if (!isset($_GET['mode']) || empty($_GET['mode']) || $_GET['mode'] === 'show') {
+    require_once 'inc/showXP.php';
+} elseif ($_GET['mode'] === 'view') {
+    require_once 'inc/viewXP.php';
+} else {
+    require_once 'inc/showXP.php';
+}
+
+require_once 'inc/footer.php';
